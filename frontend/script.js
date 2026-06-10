79f08d474ef27a75bc6dec87c849732c27f92405
AninhaSousa <aninhabtwmendes@gmail.com>
2026-06-10 17:08:10 -0300

    feat: estrutura inicial do projeto e arquivos de frontend

---
+++ frontend/script.js
@@ -0,0 +1,73 @@
+document
+.getElementById("consultaForm")
+.addEventListener("submit", async (e)=>{
+
+    e.preventDefault();
+
+    const prioridade =
+    document
+    .getElementById("prioridade")
+    .value;
+
+    const dados = {
+
+        paciente:
+        document
+        .getElementById("paciente")
+        .value,
+        
+        sintomas:
+        document
+        .getElementById("sintomas")
+        .value,
+
+        especialidade:
+        document
+        .getElementById("especialidade")
+        .value,
+
+        prioridade: prioridade
+    };
+
+    const resposta = await fetch("http://localhost:3001/agendar", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(dados)
+    });
+
+
+    const resultado =
+    await resposta.json();
+
+    const resultadoDiv =
+    document.getElementById("resultado");
+
+    /* REMOVE CLASSES ANTIGAS */
+    resultadoDiv.classList.remove(
+        "normal",
+        "urgente",
+        "emergencia"
+    );
+
+    /* DEFINE COR */
+    if(prioridade === "Normal"){
+
+        resultadoDiv.classList.add("normal");
+
+    }else if(prioridade === "Urgente"){
+
+        resultadoDiv.classList.add("urgente");
+
+    }else{
+
+        resultadoDiv.classList.add("emergencia");
+    }
+
+    resultadoDiv.innerHTML = `
+        <strong>
+            ${resultado.mensagem}
+        </strong>
+    `;
+});
